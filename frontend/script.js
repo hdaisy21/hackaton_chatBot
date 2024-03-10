90a6c440cdf57c9ea6ebf684c0c8d24bfac7e42a
hdaisy21 <61069317+hdaisy21@users.noreply.github.com>
2024-03-10 01:31:29 -0800

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -38,3 +38,10 @@ function submit(){
     };
 
 }
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en'},
+        'google_translate_element'
+    );
+}
